0494b8a247422d25e1e5db2856876436efb4736b
Mikhalchenko Boris <bmikhalchenko@beeline.kz>
2019-06-22 10:50:11 +0600

    mobile 90%

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=127.0.0.1;dbname=palata',
-    'dsn' => 'mysql:host=127.0.0.2;dbname=yii2',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=palata',
+    //'dsn' => 'mysql:host=127.0.0.2;dbname=yii2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
