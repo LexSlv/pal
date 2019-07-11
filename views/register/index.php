7be2141c15ce419090febae7cb9cd031cd9965e3
Mikhalchenko Boris <bmikhalchenko@beeline.kz>
2019-07-11 23:10:09 +0600

    + register

--- views/register/index.php
+++ views/register/index.php
@@ -9,7 +9,7 @@
     <div class="registerBox">
 
         <h2>Поиск по реестру</h2>
-        <p>Для поиска оценщика введите его фамилию и/или региональное отделение, в котором работает оценщик.</p>
+        <span>Для поиска оценщика введите его фамилию и/или региональное отделение, в котором работает оценщик.</span>
 
         <form>
 
@@ -32,14 +32,80 @@
 
                  <button class="redButton">Найти</button> 
 
-            <div>
+            </div>
 
 
         </form>
 
     </div>
+    <h2 class="red pageSubtitle">Основная информация</h2>
 
+    <table class="registerTable">
+        <tr>
+            <th>
+                Фамилия, имя, отчество
+            </th>
+            <th>
+                Номер свид-ва
+            </th>
+            <th>
+                Статус
+            </th>
+            <th>
+                Региональное отделение
+            </th>
+        </tr>
+        <tr><td></td><td></td><td></td><td></td></tr>
+        <?php $i=1; for(;$i<=10;) { print '
+        <tr>
+            <td>
+                <span class="ball blue">Иванов Иван Иванович</span>
+            </td>
+            <td>
+                <span>000000</span>
+            </td>
+            <td>
+                <span>Действительный член</span>
+            </td>
+            <td>
+                <span class="ball blue">Алматинская ППНО</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="ball blue">Иванов Иван Иванович</span>
+            </td>
+            <td>
+                <span>000000</span>
+            </td>
+            <td>
+                <span class="green">Присостановка членства</span>
+            </td>
+            <td>
+                <span class="ball blue">Алматинская ППНО</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="ball blue">Иванов Иван Иванович</span>
+            </td>
+            <td>
+                <span>000000</span>
+            </td>
+            <td>
+                <span class="red">Членство прекращено</span>
+            </td>
+            <td>
+                <span class="ball blue">Алматинская ППНО</span>
+            </td>
+        </tr>'; $i++; } ?> 
+        
 
+    </table>
+    <div class="fullWidthButton">
+        <button class="white uniButton backBlue">Показать весь список</button>
+
+    </div>
 </div>
 
 
@@ -61,33 +127,125 @@
 .registerBox form {
     display: flex;
     flex-flow: column;
+    margin: 20px 0;
+}
+
+.registerBox h2 {
+    font-size: 30px;
+    font-weight: bold;
+    line-height: 1.6;
+    margin: 10px 0;
+}
+
+.registerBox span {
+  font-size: 18px;
+  line-height: 1.333;
 }
 
 .formBox {
     width: 100%;
     display: flex;
     justify-content: space-between;
-    margin: 10px 0;
+    margin: 15px 0;
     align-items: center;
 }
 
 .formBox b {
     display: block;
     width: 20%;
+    font-size: 20px;
+    font-weight: bold;
+    line-height: 1.2;
 }
 
 .formBox input {
     width: 78%;
-    padding: 15px 10px;
+    padding: 7px 10px;
+    font-size: 18px;
+    line-height: 1.667;
+    color: #2a2d32;
 }
 
-.formBox select {
+.formBox input::placeholder {
+    color: #2a2d32;
+}
+
+.formBox select{
     width: 65%;
-    padding: 15px 10px;
+    padding: 8px 10px;
+    font-size: 18px;
+    line-height: 1.667;
+    color: #2a2d32;
+    border-radius: 0;
+    background-image: url(/web/img/arrowDown.svg);
+    background-repeat: no-repeat;
+    background-position: 98% 16px;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
 }
 
 .formBox button {
     margin: auto 0 auto auto;
 }
+
+.pageSubtitle {
+    font-size: 30px;
+    font-weight: bold;
+    line-height: 1;
+}
+
+.registerTable {
+    width: 100%;
+    border: 0;
+    border-collapse: collapse;
+}
+
+.registerTable th {
+    background-color: #f4f4f4;
+    text-align: left;
+    font-weight: 500;
+    padding: 20px 10px;
+}
+
+.registerTable td {
+    padding: 15px 10px;  
+}
+
+.ball {
+    position: relative;
+    padding-left: 1em;
+}
+
+.ball:before{
+  content: "\2022"; 
+  color: #3075b6; 
+  font-weight: bold; 
+  display: inline-block; 
+  width: 1em;
+  margin-left: -1em; 
+  position: absolute;
+}
+
+.green {
+    color: #4c9c5a;
+}
+
+.fullWidthButton {
+    width: 100%;
+    margin: 40px 0;
+}
+
+.fullWidthButton button.uniButton {
+    text-transform: unset;
+    width: 100%;
+}
+
+.fullWidthButton button.uniButton::after {
+    top: 15px;
+    right: auto;
+    margin-left: 10px;
+}
+
 
 </style>
