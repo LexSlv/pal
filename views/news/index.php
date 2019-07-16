2de44cf8f28680621c83d984a068ccc3d59b03af
Lexon <alexey.slivnev@gmail.com>
2019-07-16 23:25:22 +0600

    Вывод новостей через пагинацию news/1

--- views/news/index.php
+++ views/news/index.php
@@ -1,3 +1,8 @@
+<?php
+    use app\components\TbWidget;
+    use yii\widgets\LinkPager;
+    $lang = Yii::$app->language;
+?>
 <div class="container pageBody">
 
     <div class="breadcrumbs">
@@ -5,5 +10,26 @@
     </div>
 
     <h1 class="pageTitle red">Новости</h1>
+
+    <?php foreach ($news as $news_item): ?>
+        <div class="new">
+            <div class="smallPic" style="background-image: url(/web/uploads/news/small/<?= $news_item['image'] ?>);">
+                <a class="mask"></a>
+            </div>
+            <div class="smallText">
+                <h4><?= $news_item['title_'.$lang] ?></h4>
+                <p><?= strip_tags(substr($news_item['text_'.$lang], 0,80)) ?></p>
+                <small><?= date('d.m.Y', strtotime($news_item['date'])) ?> г.</small>
+            </div>
+        </div>
+    <?php endforeach; ?>
+
+    <hr>
+    <?php
+        echo LinkPager::widget([
+            'pagination' => $pagination,
+        ]);
+    ?>
+
 
 </div>
