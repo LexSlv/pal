f28e223f51c626824f47cab338be9d873a22e6f0
a.slivnev <alexey.slivnev@gmail.com>
2019-07-23 16:29:29 +0600

    Прописал мета скрипты на разных страницах

--- controllers/JoinController.php
+++ controllers/JoinController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace app\controllers;
+
+use Yii;
+use app\models\Meta;
+
 use yii\web\Controller;
 
 class JoinController extends Controller
@@ -8,6 +12,13 @@ class JoinController extends Controller
 
     public function actionIndex()
     {
+        $lang = Yii::$app->language;
+
+        $meta = Meta::find()->where(['alias'=>'join'])->one();
+        Yii::$app->view->params['title'] = $meta['title_'.$lang];
+        Yii::$app->view->params['description'] = $meta['description_'.$lang];
+        Yii::$app->view->params['keywords'] = $meta['keywords_'.$lang];
+
         return $this->render('index');
     }
 
--- controllers/TrainingController.php
+++ controllers/TrainingController.php
@@ -3,11 +3,21 @@
 namespace app\controllers;
 use yii\web\Controller;
 
+use Yii;
+use app\models\Meta;
+
 class TrainingController extends Controller
 {
 
     public function actionIndex()
     {
+        $lang = Yii::$app->language;
+
+        $meta = Meta::find()->where(['alias'=>'training'])->one();
+        Yii::$app->view->params['title'] = $meta['title_'.$lang];
+        Yii::$app->view->params['description'] = $meta['description_'.$lang];
+        Yii::$app->view->params['keywords'] = $meta['keywords_'.$lang];
+
         return $this->render('index');
     }
 
