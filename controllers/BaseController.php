0d41936f53c9d4d0b641d0ecc74a9087592eb9c4
a.slivnev <alexey.slivnev@gmail.com>
2019-07-24 10:59:31 +0600

    Ежеквартальная отчетность Палаты

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 use Yii;
 use yii\web\Controller;
 use app\models\Sdocs;
+use app\models\Qreports;
 use app\models\Meta;
 
 class BaseController extends Controller
@@ -19,8 +20,9 @@ class BaseController extends Controller
         Yii::$app->view->params['keywords'] = $meta['keywords_'.$lang];
 
         $sdocs = Sdocs::find()->orderBy('position ASC')->all();
+        $qreports = Qreports::find()->orderBy("date DESC")->all();
 
-        return $this->render('index',['sdocs'=>$sdocs]);
+        return $this->render('index',['sdocs'=>$sdocs, 'qreports' => $qreports]);
     }
 
 }
