3fdf41f336e56c30b8eed25077b9c7bf4ddb7c85
Mikhalchenko Boris <bmikhalchenko@beeline.kz>
2019-08-17 01:57:50 +0600

    second tab

---
+++ views/lk/payment.php
@@ -0,0 +1,8 @@
+<div class="lkPage">
+    <div class="paymentBlock">
+        <h3>Задолженность по членским взносам за Апрель 2019 года:</h3>
+        <h3 class="red">10 567 тенге</h3>
+        <button class="blueButton blue" >Оплатить</button>
+    </div>
+
+</div>
