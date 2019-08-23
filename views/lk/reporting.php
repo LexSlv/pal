e0d77981d9d19c518f53c4adcedff929e5a1a4ef
a.slivnev <alexey.slivnev@gmail.com>
2019-08-23 16:59:46 +0600

    Уведомления

--- views/lk/reporting.php
+++ views/lk/reporting.php
@@ -1,10 +1,10 @@
 <?php
-
 $lastNoticeDate = $notice[0]['date'];
-$noticeMonth = substr(date("m",strtotime($lastNoticeDate)),1);
+
+$noticeMonth = (int)date("m", strtotime($lastNoticeDate));
+
 
 isset($_GET['notice_page']) ? $nextPage = $_GET['notice_page'] + 1 : $nextPage = 1;
-
 isset($_GET['notice_page']) ? $prevPage = $_GET['notice_page'] - 1 : $prevPage = 0;
 
 ?>
@@ -13,20 +13,35 @@ isset($_GET['notice_page']) ? $prevPage = $_GET['notice_page'] - 1 : $prevPage =
 
     <div class="reportingBlock">
 
-        <h2 class="blue">Уведомления за <?= $month[$noticeMonth] ?></h2>
-
-        <?php foreach ($notice as $notice_item): ?>
-        <?php $notice_item['icon'] == 0 ? $noticeClass = "success" : $noticeClass = "warning"; ?>
-        <span class="<?= $noticeClass ?>"><?= $notice_item['massage'] ?></span>
-
-        <?php endforeach; ?>
-        <div class="reportingPagination">
-            <?php if($prevPage>=0): ?>
-            <a href="/lk/?notice_page=<?= $prevPage ?>"><span class="before">Уведомления за март</span></a>
+        <?php if (!isset($notice[0]['date'])) { ?>
+            <h1>Нет уведомлений</h1>
+            <?php if (isset($_GET['notice_page'])): ?>
+                <?php if ($_GET['notice_page']!=0): ?>
+                    <div class="reportingPagination">
+                        <a href="/lk/?notice_page=<?= $prevPage ?>"><span class="before">Назад</span></a>
+                    </div>
+                <?php endif; ?>
             <?php endif; ?>
-            <a href="/lk/?notice_page=<?= $nextPage ?>"><span class="after">Уведомления за май</span></a>
-        </div>
 
+        <?php } else { ?>
+
+            <h2 class="blue">Уведомления за <?= $month[$noticeMonth] ?></h2>
+
+            <?php foreach ($notice as $notice_item): ?>
+                <?php $notice_item['icon'] == 0 ? $noticeClass = "success" : $noticeClass = "warning"; ?>
+                <span class="<?= $noticeClass ?>"><?= $notice_item['massage'] ?></span>
+            <?php endforeach; ?>
+
+
+            <div class="reportingPagination">
+                <?php if ($prevPage >= 0): ?>
+                <a href="/lk/?notice_page=<?= $prevPage ?>"><span class="before">Уведомления за <?= $month[$noticeMonth + 1] ?></span></a>
+                <?php endif; ?>
+                <a href="/lk/?notice_page=<?= $nextPage ?>"><span class="after">Уведомления за <?= $month[$noticeMonth - 1] ?></span></a>
+
+            </div>
+        <?php } ?>
     </div>
 
 </div>
+
